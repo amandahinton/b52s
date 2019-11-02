18cc457eda74657550043023c122f2c6f2097d94
amandahinton <dawn2681@gmail.com>
2019-11-02 02:31:51 -0700

    gallery and shop

---
+++ index.js
@@ -0,0 +1,23 @@
+$ (document).ready(function() {
+    //ADD SOME CODE
+    //alert("Hello friend!!")
+    //document.getElementById('demo').innerHTML = "This is the new text";
+
+    //$("h2").click(function() {
+       // $(this).removeClass("highlight")
+   // });
+
+    //$("h1").click(function() {
+     //   $(this).toggleClass("highlight")
+    //});
+
+   // $("#al-dismiss").click(function() {
+    //    $('#myalert').hide()
+   // });
+
+    $('.splashtoggle').on('click', function () {
+        $('#portraitswap').toggle();
+    })
+
+});
+
