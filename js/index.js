447f5e0a43b6a76e4db7aca150ab0bff58effd5c
amandahinton <dawn2681@gmail.com>
2019-11-15 22:38:21 -0800

    js shop filters + page text

--- js/index.js
+++ js/index.js
@@ -15,7 +15,47 @@ $(document).ready(function() {
   });
 
 
+  $("#apparellink").click(function() {
+    $(".apparelcat").show();
+    $(".musiccat").hide();
+    $(".thingeecat").hide();
+    $("#apparellink").addClass("ahactivecat");
+    $("#alllink").removeClass("ahactivecat");
+    $("#musiclink").removeClass("ahactivecat");
+    $("#thingeeslink").removeClass("ahactivecat");
+    
 
+  });
+
+  $("#musiclink").click(function() {
+    $(".apparelcat").hide();
+    $(".musiccat").show();
+    $(".thingeecat").hide();
+    $("#musiclink").addClass("ahactivecat");
+    $("#alllink").removeClass("ahactivecat");
+    $("#apparellink").removeClass("ahactivecat");
+    $("#thingeeslink").removeClass("ahactivecat");
+  });
+
+  $("#thingeeslink").click(function() {
+    $(".apparelcat").hide();
+    $(".musiccat").hide();
+    $(".thingeecat").show();
+    $("#thingeeslink").addClass("ahactivecat");
+    $("#alllink").removeClass("ahactivecat");
+    $("#apparellink").removeClass("ahactivecat");
+    $("#musiclink").removeClass("ahactivecat");
+  });
+
+  $("#alllink").click(function() {
+    $(".apparelcat").show();
+    $(".musiccat").show();
+    $(".thingeecat").show();
+    $("#alllink").addClass("ahactivecat");
+    $("#musiclink").removeClass("ahactivecat");
+    $("#apparellink").removeClass("ahactivecat");
+    $("#thingeeslink").removeClass("ahactivecat");
+  });
 
 });
 
